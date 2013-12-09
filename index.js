3b4f89d39b827075a67952614f5c4aef323a53cf
Sebastien Ballesteros <sebastien.ballesteros@gmail.com>
2013-12-09 18:33:07 -0500

    user proxy

--- index.js
+++ index.js
@@ -8,46 +8,21 @@ var Dpm = module.exports = function(rc, root){
   this.rc = rc;
 };
 
-['adduser','publish', 'install'].forEach(function(method){
+['adduser','publish', 'install', 'request'].forEach(function(method){
   Dpm.prototype[method] = require('./lib/' + method);
 });
+
 
 Dpm.prototype.lsOwner = owner.ls;
 Dpm.prototype.addOwner = owner.add;
 Dpm.prototype.rmOwner = owner.rm;
 
 /**
- * get an auth token
+ * data: {dpkgName[@version]}
  */
-Dpm.prototype.auth = function(callback){
+Dpm.prototype.unpublish = function(dpkgId, callback){
+  dpkgId = dpkgId.replace('@', '/');
 
-  if('token' in this.rc){
-    return callback(null, this.rc.token);
-  }
-
-  var data = JSON.stringify({name: this.rc.name, password: this.rc.password});
-
-  var options = {
-    port: 5984,
-    hostname: '127.0.0.1',
-    method: 'POST',
-    path: '/_session',
-    headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': Buffer.byteLength(data,'utf8')
-    }
-  };
-
-  http.request(options, function(res){
-    try {
-      var token = cookie.parse(res.headers['set-cookie'][0])['AuthSession'];
-    } catch(e){
-      return callback(new Error('no cookie for auth: ' + e.message));
-    }
-    res.resume();
-
-    this.rc.token = token;
-    callback(null, token);
-  }.bind(this)).end(data);
-
+  this.request('/unpublish/'+ dpkgId, callback);
 };
+

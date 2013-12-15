a2682efbbe4655a0157deac8bd4d3e56bf38934c
Sebastien Ballesteros <sebastien.ballesteros@gmail.com>
2013-12-14 23:09:31 -0500

    clone, get, cat, install

--- README.md
+++ README.md
@@ -5,24 +5,41 @@ A client (```dpm```) for the [data registry](https://github.com/standard-analyti
 
 [![NPM](https://nodei.co/npm/dpm-stan.png)](https://nodei.co/npm/dpm-stan/)
 
+
 Usage:
 ======
 
+
     $ dpm --help
     
-    dpm <command> [options] where command is:
-      - install   <datapackage name>[@<version>] [-f, --force] [-c, --cache]
+    Usage: dpm <command> [options] where command is:
+      - cat       <datapackage name>[@<version>]
+        Display package.json on stdout. If @<version> is not specified, the latest version is returned.
+      - get      <datapackage name>[@<version>] [-f, --force] [-c, --cache]
+        Download a data package into a directory (named after the data package name).
+      - clone    <datapackage name>[@<version>] [-f, --force]
+        Download a data package into a directory (named after the data package name) reproducing the directory structure present at publication time (including any files or folder not listed as resources (e.g scripts/ ...)).
+      - install   <datapackage name 1>[@<version>] <datapackage name 2>[@<version>] ... [-c, --cache] [-s, --save]
+        Installs a list of data packages, and any data packages that they depends on as dependencies of your current project (i.e in a directory named "data_modules")
       - publish
+        Publish the data package of the current working directory into the registry
       - unpublish <datapackage name>[@<version>]
+        Unpublish a datapackage from the registry. If no version is specified, all the versions will be removed
       - adduser
+        Register an user
       - owner <subcommand> where subcommand is:
         - ls  <datapackage name>
+          List all the maintainers of the data package
         - add <user> <datapackage name>
+          Add an user to the data package maintainers
         - rm  <user> <datapackage name>[@<version>]
+          Remove an user from the data package maintainers
       - search [search terms]
+        Search data packages by keywords
     
     Options:
       -f, --force    just do it
+      -s, --save     data packages will appear in your dataDependencies
       -c, --cache    store the resources content on the disk in a data/ directory
       -h, --help     print usage
       -v, --version  print version number
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var http = require('http')
   , crypto = require('crypto')
+  , semver = require('semver')
   , querystring = require('querystring')
   , cookie = require('cookie')
   , EventEmitter = require('events').EventEmitter
@@ -7,8 +8,12 @@ var http = require('http')
   , request = require('request')
   , path = require('path')
   , mime = require('mime')
+  , rimraf = require('rimraf')
+  , mkdirp = require('mkdirp')
   , async = require('async')
   , fs = require('fs')
+  , zlib = require('zlib')
+  , tar = require('tar')
   , publish = require('./lib/publish');
 
 
@@ -33,7 +38,7 @@ Dpm.prototype.auth = function(){
   return {user: this.rc.name, pass: this.rc.password};
 }
 
-Dpm.prototype.log = function(methodCode, reqUrl){
+Dpm.prototype.logHttp = function(methodCode, reqUrl){
   this.emit('log', 'dpm'.grey + ' http '.green + methodCode.toString().magenta + ' ' + reqUrl.replace(':80/', '/'));
 };
 
@@ -41,11 +46,11 @@ Dpm.prototype.log = function(methodCode, reqUrl){
 Dpm.prototype.lsOwner = function(dpgkName, callback){
 
   var rurl = this.url('/owner/ls/' + dpkgName);
-  this.log('GET', rurl);
+  this.logHttp('GET', rurl);
 
   request(rurl, function(err, res, body){
     if(err) return callback(err);
-    this.log(res.statusCode, rurl);
+    this.logHttp(res.statusCode, rurl);
 
     if(res.statusCode >= 400){
       var err = new Error(body);
@@ -63,14 +68,14 @@ Dpm.prototype.lsOwner = function(dpgkName, callback){
  */
 Dpm.prototype.addOwner = function(data, callback){
   var rurl = this.url('/owner/add');
-  this.log('POST', rurl);
+  this.logHttp('POST', rurl);
   request.post({
     url: rurl,
     auth: this.auth(),
     json: data
   }, function(err, res, body){
     if(err) return callback(err);
-    this.log(res.statusCode, rurl);
+    this.logHttp(res.statusCode, rurl);
     if(res.statusCode >= 400){
       var err = new Error(JSON.stringify(body));
       err.code = res.statusCode;
@@ -85,14 +90,14 @@ Dpm.prototype.addOwner = function(data, callback){
  */
 Dpm.prototype.rmOwner = function(data, callback){
   var rurl = this.url('/owner/rm');
-  this.log('POST', rurl);
+  this.logHttp('POST', rurl);
   request.post({
     url: rurl,
     auth: this.auth(),
     json: data
   }, function(err, res, body){
     if(err) return callback(err);
-    this.log(res.statusCode, rurl);
+    this.logHttp(res.statusCode, rurl);
     if(res.statusCode >= 400){
       var err = new Error(JSON.stringify(body));
       err.code = res.statusCode;
@@ -110,13 +115,13 @@ Dpm.prototype.unpublish = function(dpkgId, callback){
   dpkgId = dpkgId.replace('@', '/');
 
   var rurl = this.url('/'+ dpkgId);
-  this.log('DELETE', rurl);
+  this.logHttp('DELETE', rurl);
   request.del({
     url: rurl,
     auth: this.auth()
   }, function(err, res, body){
     if(err) return callback(err);
-    this.log(res.statusCode, rurl);
+    this.logHttp(res.statusCode, rurl);
     if(res.statusCode >= 400){
       var err = new Error(body);
       err.code = res.statusCode;
@@ -128,87 +133,243 @@ Dpm.prototype.unpublish = function(dpkgId, callback){
 };
 
 
-/**
- * opts: {cache:}
- */
-Dpm.prototype.install = function(what, opts, callback){
+Dpm.prototype.cat = function(dpkgId, opts, callback){
 
   if(arguments.length === 2){
     callback = opts;
     opts = {};
   }
-  var rurl = this.url('/' + what.datapackage + (('version' in what) ?  ('/' + what.version) : ''));
-  this.log('GET', rurl);
+
+  var splt = dpkgId.split('@');
+  if(splt.length === 2){
+    var version = semver.valid(splt[1]);
+    if(!version){
+      return callback(new Error('invalid version '+ dpkgId.red +' see http://semver.org/'));
+    }
+  }
+
+  var rurl = this.url('/' + dpkgId.replace('@', '/'), (opts.clone) ? {clone:true} : undefined);
+  this.logHttp('GET', rurl);
 
   request(rurl, function(err, res, dpkg){
     if(err) return callback(err);
 
-    this.log(res.statusCode, rurl);
+    this.logHttp(res.statusCode, rurl);
     if (res.statusCode >= 300){
       var err = new Error('fail');
       err.code = res.statusCode;
       return callback(err);
     }
 
-    var dpkg = JSON.parse(dpkg);
+    callback(null, JSON.parse(dpkg));
 
-    if(opts.cache && dpkg.resources.length){
-
-      var dataPath = path.join(this.root, 'data');
-      fs.mkdir(dataPath, function(err){
-        if(err) return callback(err);
-        async.each(dpkg.resources, function(r, cb){
-          if('url' in r){
-            this.log('GET', r.url);
-            var req = request(r.url);
-            req.on('response', function(resp){            
-              var hasCallbacked = false;
-
-              this.log(resp.statusCode, r.url);
-
-              if(resp.statusCode >= 400){
-                resp.pipe(process.stdout);
-              } else {
-                var filename =  r.name + '.' +mime.extension(resp.headers['content-type']);
-                resp
-                  .on('error', function(err){
-                    if(!hasCallbacked){
-                      hasCallbacked = true;
-                      cb(err);
-                    }
-                  })
-                  .pipe(fs.createWriteStream(path.join(dataPath, filename)))
-                  .on('finish', function(){
-                    //replace url by the path
-                    delete r.url;
-                    r.path = path.join('data', filename);
-
-                    if(!hasCallbacked) cb(null);
-                  });
-              }              
-            }.bind(this));
-          } else {
-            cb(null);
-          }                
-        }.bind(this), function(err, _){
-          callback(null, dpkg);        
-        });
-      }.bind(this));
-
-    } else {
-      callback(null, dpkg);
-    }
-    
   }.bind(this));
 
 };
 
 
 
+
+
+
+
+Dpm.prototype.get = function(dpkgId, opts, callback){
+
+  if(arguments.length === 2){
+    callback = opts;
+    opts = {};
+  }
+
+  opts.root = opts.root || this.root; 
+  opts.root = path.join(opts.root, dpkgId.split('@')[0]);
+
+  async.waterfall([
+    function(cb){
+      _createDir(opts.root, opts, function(err){
+        cb(err);//make sure arrity of cb is 1
+      });
+    },
+    function(cb){
+
+      this.cat(dpkgId, function(err, dpkg){
+        if(err) return cb(err);
+        var dest = path.join(opts.root, 'package.json');
+
+        if(opts.cache){
+          this._cache(dpkg, opts, cb);
+        } else {
+          fs.writeFile(dest, JSON.stringify(dpkg, null, 2), function(err){
+            if(err) return cb(err);
+            cb(null, dpkg);
+          });
+        }
+        
+      }.bind(this));
+      
+    }.bind(this)
+  ], callback);
+  
+};
+
+
+Dpm.prototype._cache = function(dpkg, opts, callback){
+
+  if(arguments.length === 2){
+    callback = opts;
+    opts = {};
+  }
+  opts.root = opts.root || this.root;
+
+  var resources = dpkg.resources.filter(function(r){return 'url' in r;});
+  if(opts.clone) {
+    resources = resources.filter(function(r){return 'path' in r;});
+  }
+  
+  async.each(resources, function(r, cb){
+
+    var root;
+    if(opts.clone){
+      root = path.resolve(opts.root, path.dirname(r.path));
+    } else {
+      root = path.resolve(opts.root, 'data');
+    }
+
+    mkdirp(root, function(err){
+
+      if(err) return callback(err);
+
+      this.logHttp('GET', r.url);
+      var req = request(r.url);
+      req.on('response', function(resp){            
+        var hasCallbacked = false;
+
+        this.logHttp(resp.statusCode, r.url);
+
+        if(resp.statusCode >= 400){
+          resp.pipe(process.stdout);
+        } else {
+
+          var filename = (opts.clone)? path.basename(r.path) : r.name + '.' +mime.extension(resp.headers['content-type']);
+
+          resp
+            .on('error', function(err){
+              if(!hasCallbacked){
+                hasCallbacked = true;
+                cb(err);
+              }
+            })
+            .pipe(fs.createWriteStream(path.join(root, filename)))
+            .on('finish', function(){
+              if(!opts.clone){
+                r.path = path.join('data', filename);
+              }
+              delete r.url;
+              
+              if(!hasCallbacked) cb(null);
+            });
+        }
+      }.bind(this));
+
+    }.bind(this));
+
+  }.bind(this), function(err){
+
+    if(err) return callback(err);
+    fs.writeFile(path.join(opts.root, 'package.json'), JSON.stringify(dpkg, null, 2), function(err){
+      if(err) return callback(err);
+      callback(null, dpkg);
+    });
+
+  });
+
+};
+
+
+Dpm.prototype.clone = function(dpkgId, opts, callback){
+
+  if(arguments.length === 2){
+    callback = opts;
+    opts = {};
+  }
+
+  this.cat(dpkgId, {clone:true}, function(err, dpkg){
+
+    var root = path.join(this.root, dpkg.name);
+    _createDir(root, opts, function(err){
+      if(err) {
+        return callback(err);
+      }
+
+      var rurl = this.url('/' + dpkg.name + '/' + dpkg.version + '/debug');
+      this.logHttp('GET', rurl);
+
+      var req = request(rurl);
+      req.on('response', function(resp){            
+        var hasCallbacked = false;
+
+        this.logHttp(resp.statusCode, rurl);
+
+        if(resp.statusCode >= 400){
+          resp.pipe(process.stdout);
+        } else {
+
+          resp
+            .on('error', function(err){
+              if(!hasCallbacked){
+                hasCallbacked = true;
+                callback(err);
+              }
+            })
+            .pipe(zlib.createGunzip())
+            .pipe(new tar.Extract({
+              path: root,
+              strip: 1
+            }))
+            .on('end', function(){
+
+              this._cache(dpkg, {clone: true, force: opts.force, root: root}, function(err, dpkg){              
+                if(!hasCallbacked){
+                  callback(err, dpkg);
+                }
+              });
+
+            }.bind(this));
+
+        }
+      }.bind(this));    
+
+    }.bind(this));
+
+  }.bind(this));
+  
+};
+
+
+Dpm.prototype.install = function(dpkgIds, opts, callback){
+
+  if(arguments.length === 2){
+    callback = opts;
+    opts = {};
+  }
+  
+  async.map(dpkgIds, function(dpkgId, cb){
+
+    var root = path.join(this.root, 'data_modules');    
+
+    this.get(dpkgId, {cache: opts.cache, root: root, force: opts.force}, function(err, dpkg){
+      if(err) return cb(err);
+      cb(null, {name: dpkg.name, version:dpkg.version});
+    });
+    
+  }.bind(this), callback);
+
+};
+
+
 Dpm.prototype.adduser = function(callback){
 
   var rurl = this.url('/adduser/' + this.rc.name);
-  this.log('PUT', rurl);
+  this.logHttp('PUT', rurl);
 
   var data = {
     name: this.rc.name,
@@ -231,7 +392,7 @@ Dpm.prototype.adduser = function(callback){
 
     if(err) return callback(err);
 
-    this.log(res.statusCode, rurl);
+    this.logHttp(res.statusCode, rurl);
 
     if(res.statusCode < 400){
       callback(null, body);
@@ -246,5 +407,25 @@ Dpm.prototype.adduser = function(callback){
     }
     
   }.bind(this));
+
+};
+
+
+function _createDir(dirPath, opts, callback){
+
+  fs.exists(dirPath, function(exists){
+    if(exists){
+      if(opts.force) {
+        rimraf(dirPath, function(err){
+          if(err) return callback(err);
+          mkdirp(dirPath, callback);                
+        });
+      } else {
+        callback(new Error(dirPath + ' already exists, run with --force to overwrite'));
+      }
+    } else {
+      mkdirp(dirPath, callback);      
+    }
+  });
 
 };

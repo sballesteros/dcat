7d80261aca9c481037905976e14090d6d7b1e152
JDureau <dureau.joseph@gmail.com>
2014-04-21 19:04:13 +0200

    separate test file for plugin

--- test/test.js
+++ test/test.js
@@ -8,8 +8,7 @@ var util = require('util')
   , readdirpSync = require('fs-readdir-recursive')
   , _ = require('underscore')
   , exec = require('child_process').exec
-  , path = require('path')
-  , pone = require('../plugin/pone');
+  , path = require('path');
 
 temp.track();
 
@@ -484,43 +483,6 @@ describe('ldpm', function(){
         });
       });
     });
-  });
-
-
-  describe('pone', function(){
-
-    it('should return a pkg with name haseleu-2014 when asked for finger-wrinkles paper through url', function(done){
-      var ldpm = new Ldpm(conf);
-      ldpm.markup('plosone', 'http://www.plosone.org/article/info%3Adoi%2F10.1371%2Fjournal.pone.0084949', function(err,pkg){
-        assert.equal(pkg.name,'haseleu-2014');
-        done();
-      });
-    });
-
-    it('should return a pkg with name haseleu-2014 when asked for finger-wrinkles paper through doi', function(done){
-      var ldpm = new Ldpm(conf);
-      ldpm.markup('plosone', '10.1371/journal.pone.0084949', function(err,pkg){
-        assert.equal(pkg.name,'haseleu-2014');
-        done();
-      });
-    });
-
-    it('should error 404 if article does not exist', function(done){
-      var ldpm = new Ldpm(conf);
-      ldpm.markup('plosone','http://www.plosone.org/article/info%3Adoi%2F10.1371%2Fjournal.pone.9999999', function(err,pkg){
-        assert.equal(err.code, '404');
-        done();
-      });
-    });
-
-    it('should error 400 if url does not start with http://www.plosone.org/article/info', function(done){
-      var ldpm = new Ldpm(conf);
-      ldpm.markup('plosone','http://google.com', function(err,pkg){
-        assert.equal(err.code, '400');
-        done();
-      });
-    });
-
   });
 
 });

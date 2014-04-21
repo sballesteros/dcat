2512d9573431351e5dac2a8d6b881312ff4f77b7
sebastien ballesteros <sebastien.ballesteros@gmail.com>
2014-04-21 15:02:04 -0400

    fix test pone + release 0.4.1

--- test/test-pone.js
+++ test/test-pone.js
@@ -24,7 +24,7 @@ describe('pone', function(){
   it('should return a pkg with name haseleu-2014 when asked for finger-wrinkles paper through url', function(done){
     var ldpm = new Ldpm(conf);
     ldpm.markup('plosone', 'http://www.plosone.org/article/info%3Adoi%2F10.1371%2Fjournal.pone.0084949', function(err,pkg){
-      assert.equal(pkg.name,'haseleu-2014');
+      assert.equal(pkg.name,'pone-haseleu-2014');
       done();
     });
   });
@@ -32,7 +32,7 @@ describe('pone', function(){
   it('should return a pkg with name haseleu-2014 when asked for finger-wrinkles paper through doi', function(done){
     var ldpm = new Ldpm(conf);
     ldpm.markup('plosone', '10.1371/journal.pone.0084949', function(err,pkg){
-      assert.equal(pkg.name,'haseleu-2014');
+      assert.equal(pkg.name,'pone-haseleu-2014');
       done();
     });
   });
